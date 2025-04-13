0644501b05e3f092a2a45a3b4e05e44a4816d6c5
GazdagB <gazdagbal@gmail.com>
2025-04-13 22:31:45 +0200

    ✨ Adds recursive to js

--- src/algorithms/search/binary/js/binarySearch.js
+++ src/algorithms/search/binary/js/binarySearch.js
@@ -31,8 +31,38 @@ function binarySearch(arr, targetValue){
     return -1; 
 }
 
+/**
+ * 
+ * @param {Array} arr 
+ * @param {Number} targetValue 
+ * @param {Number} left 
+ * @param {Number} right 
+ * @returns {Number}
+ */
+function binaryRecursive(arr,targetValue,left,right){
+
+    if(left > right){
+        return -1
+    } 
+    
+    let mid = Math.floor((left + right) / 2); 
+
+    if(arr[mid] === targetValue){
+        return mid; 
+    }
+
+    if(arr[mid] > targetValue){
+        return  binaryRecursive(arr,targetValue,left, mid -1);
+    }
+
+    return binaryRecursive(arr,targetValue, mid + 1, right); 
+    
+
+}
+
 const sortedArray = [1,2,5,9,16,28,47,64,78,99,120,158,225]
-const targetValue = 120; 
+const targetValue = 2; 
 
 
 console.log(binarySearch(sortedArray,targetValue));
+console.log(binaryRecursive(sortedArray,targetValue,0,sortedArray.length - 1))
